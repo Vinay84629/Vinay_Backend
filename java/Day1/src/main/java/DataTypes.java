2afba63e995d1bc57f35dbe2198edd88adb6c173
Vinay kumar T N <vinaykumartn2910@gmail.com>
2024-11-07 20:28:35 +0530

    java fundamentals

--- java/Day1/src/main/java/DataTypes.java
+++ java/Day1/src/main/java/DataTypes.java
@@ -1,2 +1,22 @@
-public class DataTypes {
+public class DataTypes
+{
+    public static void main (String args[])
+    {
+        byte b = 1;
+        short s = 2;
+        int in = 22;
+        long number = 1230L;
+        float ft = 10.22f;
+        double db = 103.122;
+        char ch = 'A';
+        boolean bn = true;
+
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + in);
+        System.out.println("float : " + number);
+        System.out.println("double : " + db);
+        System.out.println("char : " + ch);
+        System.out.println("boolean : " + bn);
+    }
 }
---
+++ java/Day1/src/main/java/TypeConversion.java
@@ -0,0 +1,10 @@
+public class TypeConversion {
+        public static void main (String args[])
+        {
+        // Write a Java program to convert a double to an int and explain the result.
+            double height = 5.925;
+            int height1 = (int) height;
+            System.out.println("Double value : " + height);
+            System.out.println("double value after converted into int : " + height1);
+        }
+}
