4465319e7da7b0dcd6a9c691de73d9b2c61b2dac
Vinay kumar T N <vinaykumartn2910@gmail.com>
2024-11-07 21:40:52 +0530

    jave variables

--- java/Day1/src/main/java/Variables.java
+++ java/Day1/src/main/java/Variables.java
@@ -1,9 +1,13 @@
-public class Variables
-{
+public class Variables {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int sum = (a+b);
-        System.out.println(sum);
+        //Write a Java program to declare variables of different types (int, double, String), assign values to them, and print the values
+
+        int age = 26;
+        double salary = 26000.36;
+        String name = "Alice";
+
+        System.out.println("Name of the user : " + name);
+        System.out.println("Age of alice : " + age);
+        System.out.println("salary of alice : " + salary);
     }
 }
