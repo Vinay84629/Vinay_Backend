df7b4904fc58390bff8c83785683ad7127633a76
Vinay kumar T N <vinaykumartn2910@gmail.com>
2024-11-09 17:35:46 +0530

    String Methods

--- java/Day02/src/main/java/StringMethods.java
+++ java/Day02/src/main/java/StringMethods.java
@@ -1,13 +1,18 @@
 public class StringMethods {
     public static void main(String[] args) {
         String str = new String();
-        str = "Hello World";
+        str = "Hello World ";
 
         int len = str.length();
+        //will consider white spaces also
         String trim = str.trim();
         String upper = str.toUpperCase();
         String lower = str.toLowerCase();
         String sub1 = str.substring(2,10);
-        System.out.println(lower);
+
+        // Replace
+        String replace1 = str.replace('e','o');
+        String replace2 = str.replace("World","Guddi pie");
+        System.out.println(replace2);
     }
 }
